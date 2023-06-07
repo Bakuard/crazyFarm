5eb9abda43c6d88aca5b5b1c8ae628edfa7ab9cf
Bakuard <purplespicemerchant@gmail.com>
2023-06-07 14:08:03 +0600

    Настроен CORS

--- src/code/index.js
+++ src/code/index.js
@@ -17,6 +17,12 @@ const app = express();
 
 app.use(i18nMiddleware.handle(i18next));
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use(function (req, res, next) {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.post('/users/enter', express.json(), wrapAsync(userController.enter.bind(userController)));
 app.post('/users/registration/firstStep', express.json(), wrapAsync(userController.registrationFirstStep.bind(userController)));
 app.post('/users/registration/finalStep', wrapAsync(userController.registrationFinalStep.bind(userController)));
