c8e43de5c0218fa887022fad3e4afcaf653a0a5a
Bakuard <purplespicemerchant@gmail.com>
2023-06-14 14:20:09 +0600

    добавлено удаление мертвых websocket соединений

--- src/code/controller/gameController.js
+++ src/code/controller/gameController.js
@@ -10,10 +10,21 @@ module.exports.GameController = class GameController {
 
     #jwsService;
     #wsServer;
+    #intervalIdForPing;
 
     constructor(jwsService, wsServer) {
         this.#jwsService = jwsService;
         this.#wsServer = wsServer;
+        this.#intervalIdForPing = setInterval(() => {
+            wsServer.clients.forEach(clientSocket => {
+                if(clientSocket.isAlive === false) {
+                    clientSocket.terminate();
+                    logger.info(`terminate client socket with userId=${clientSocket.userId}`);
+                }
+                clientSocket.isAlive = false;
+                clientSocket.ping();
+            });
+        }, process.env.WEBSOCKET_PING_TIMEOUT_IN_MS);
     }
 
     async getJwtForConnection(req, res, next) {
@@ -26,8 +37,10 @@ module.exports.GameController = class GameController {
         clientSocket.on('message', data => {});
         clientSocket.on('error', e => logger.error(e));
         clientSocket.on('close', () => {});
+        clientSocket.on('pong', () => clientSocket.isAlive = true);
 
         clientSocket.userId = req.userId;
+        clientSocket.isAlive = true;
         clientSocket.send(`You have been connected to CrazyFarm websocket. User id=${req.userId}`);
     }
 
@@ -39,7 +52,7 @@ module.exports.GameController = class GameController {
             try {
                 let userId = this.#jwsService.parseJws(jws, 'websocket');
                 req.userId = userId;
-                this.#wsServer.handleUpgrade(req, socket, head, ws => wsServer.emit('connection', ws, req));
+                this.#wsServer.handleUpgrade(req, socket, head, ws => this.#wsServer.emit('connection', ws, req));
             } catch(err) {
                 logger.error(`Faile to connect by websocket. Reason: %s`, err.message);
                 socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
