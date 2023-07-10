05759dc49ef7ac56b09b6f3831c2cff4ae951a80
Bakuard <purplespicemerchant@gmail.com>
2023-07-10 15:26:56 +0600

    Найдена причина бага связанного с выкапыванием овоща

--- test/model/logic/shovel.test.js
+++ test/model/logic/shovel.test.js
@@ -4,6 +4,8 @@ const {EntityComponentManager} = require('../../../src/code/model/gameEngine/ent
 const {ComponentIdGenerator} = require('../../../src/code/model/gameEngine/componentIdGenerator.js');
 const {EntityManager} = require('../../../src/code/model/gameEngine/entityManager.js');
 const {EventManager} = require('../../../src/code/model/gameEngine/eventManager.js');
+const {VegetableMeta} = require('../../../src/code/model/logic/vegetableMeta.js');
+const {GardenBedCellLink} = require('../../../src/code/model/logic/gardenBedCellLink.js');
 
 let manager = null;
 let eventManager = null;
@@ -32,7 +34,8 @@ test(`update(groupName, world):
     });
 
 test(`update(groupName, world):
-        there are not 'shovel' events
+        there is 'shovel' event,
+        gardenBedCell is empty
         => do nothing`,
     () => {
         let entity = manager.createEntity();
@@ -49,4 +52,50 @@ test(`update(groupName, world):
         system.update('update', worldMock);
 
         expect(entity).toEqualEntity(expectEntity);
+    });
+
+test(`update(groupName, world):
+        there is 'shovel' event,
+        gardenBedCell is not empty
+        => remove vegetable`,
+    () => {
+        let cell = manager.createEntity().put(GardenBedCell.of(0, 0));
+        let vegetable = manager.createEntity().addTags('sleeping seed').put(new VegetableMeta('Potato'), new GardenBedCellLink(cell));
+        cell.get(GardenBedCell).vegetable = vegetable;
+        manager.bindEntity(cell);
+        manager.bindEntity(vegetable);
+        eventManager.writeEvent('shovel', {tool: 'shovel', cell: 'center'});
+        let worldMock = {
+            getEntityComponentManager: () => manager,
+            getEventManager: () => eventManager
+        };
+
+        let system = new ShovelSystem(manager);
+        system.update('update', worldMock);
+        let actual = cell.get(GardenBedCell).vegetable;
+
+        expect(actual).toBeNull();
+    });
+
+test(`update(groupName, world):
+        there is 'shovel' event,
+        gardenBedCell is not empty
+        => isAlive(vegetable) must return false`,
+    () => {
+        let cell = manager.createEntity().put(GardenBedCell.of(0, 0));
+        let vegetable = manager.createEntity().addTags('sleeping seed').put(new VegetableMeta('Potato'), new GardenBedCellLink(cell));
+        cell.get(GardenBedCell).vegetable = vegetable;
+        manager.bindEntity(cell);
+        manager.bindEntity(vegetable);
+        eventManager.writeEvent('shovel', {tool: 'shovel', cell: 'center'});
+        let worldMock = {
+            getEntityComponentManager: () => manager,
+            getEventManager: () => eventManager
+        };
+
+        let system = new ShovelSystem(manager);
+        system.update('update', worldMock);
+        let actual = manager.isAlive(vegetable);
+
+        expect(actual).toBe(false);
     });
